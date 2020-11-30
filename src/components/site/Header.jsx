2ab97f31686a85348e50ab33aa709aa245a403f1
Robin Pillar <rpillar72@gmail.com>
2020-11-30 21:55:41 +0000

    changed header nav links to copy type style

--- src/components/site/Header.jsx
+++ src/components/site/Header.jsx
@@ -5,22 +5,22 @@ export default function Header() {
   return (
     <section className='site-header'>
       <div className='flex px-4 space-x-4'>
-        <Link className='mr-auto cursor-trigger py-4 tagline' to='/'>
+        <Link className='mr-auto cursor-trigger py-4 copy' to='/'>
           Robin Pillar
         </Link>
-        <Link className='cursor-trigger py-4 tagline' to='/about'>
+        <Link className='cursor-trigger py-4 copy' to='/about'>
           About
         </Link>
-        <Link className='cursor-trigger py-4 tagline' to='/skills'>
+        <Link className='cursor-trigger py-4 copy' to='/skills'>
           Skills
         </Link>
-        <Link className='cursor-trigger py-4 tagline' to='/experience'>
+        <Link className='cursor-trigger py-4 copy' to='/experience'>
           Experience
         </Link>
-        <Link className='cursor-trigger py-4 tagline' to='/projects'>
+        <Link className='cursor-trigger py-4 copy' to='/projects'>
           Projects
         </Link>
-        <Link className='cursor-trigger py-4 tagline' to='/contact'>
+        <Link className='cursor-trigger py-4 copy' to='/contact'>
           Contact
         </Link>
       </div>
