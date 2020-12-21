23407c9dc24a2220c320508c4d1febe638fdef39
Robin Pillar <rpillar72@gmail.com>
2020-12-21 13:12:37 +0000

    readded modal

--- src/components/home/Modal.jsx
+++ src/components/home/Modal.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-import { useDispatch } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
 import { toggleModal } from '../../store/actions/app';
 
-export default function Modal() {
+import { ReactComponent as Cross } from '../../assets/svg/cross.svg';
+
+import gsap from 'gsap';
+import { isNull } from 'lodash';
+
+export default function PageModal() {
+  const { modal } = useSelector((state) => state.app);
   const dispatch = useDispatch();
+  const modalRef = useRef();
+
+  useEffect(() => {
+    if (modal && !isNull(modalRef)) {
+      gsap.to(modalRef.current, { scale: 1, duration: 0.3 });
+      if (!window.matchMedia('(min-width:1025px)').matches)
+        gsap.to(modalRef.current, {
+          borderRadius: 0,
+          duration: 0.3,
+          delay: 0.3,
+        });
+    } else if (!modal && !isNull(modalRef)) {
+      gsap.to(modalRef.current, { scale: 0, duration: 0.3 });
+    }
+  }, [modal]);
 
   const onClick = () => {
     dispatch(toggleModal(false));
   };
+
   return (
-    <section className='home-modal w-screen h-screen fixed inset-0 flex items-center justify-center'>
-      <div className='container z-30'>
-        <div className='h-120 w-full bg-white rounded-20'></div>
-      </div>
-      <div
-        className='absolute bg-black opacity-50 inset-0 z-10'
+    <section
+      ref={modalRef}
+      className='home-pageModal fixed inset-0 bg-white transform scale-0 rounded-20 xl:m-8 z-30 cursor-dark p-4 overflow-hidden'
+    >
+      <button
+        className='home-pageModal__button absolute top-0 left-0 rounded-full mt-4 ml-4 h-10 min-h-10 w-10 min-w-10 text-white bg-gradient-to-tr from-indigo-600 to-indigo-400 cursor-trigger'
         onClick={onClick}
-      ></div>
+      >
+        <Cross />
+      </button>
     </section>
   );
 }
