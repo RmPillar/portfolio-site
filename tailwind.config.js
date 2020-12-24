7d1d77b37af451370aaa8ffedd7ac4461aadd66a
Robin Pillar <rpillar72@gmail.com>
2020-12-24 14:12:12 +0000

    updated and ran production

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./src/**/*.jsx'],
   theme: {
     screens: {
       sm: '300px',
