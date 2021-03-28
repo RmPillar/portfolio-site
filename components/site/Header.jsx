913d76f54699cf92b8565858ca5657153a1c088d
Robin Pillar <rpillar72@gmail.com>
2021-03-28 16:45:22 +0100

    fixed stylings for header and mobile menu

--- components/site/Header.jsx
+++ components/site/Header.jsx
@@ -5,8 +5,8 @@ import BtnMenu from '../buttons/BtnMenu';
 export default function Header() {
   return (
     <section className='site-header fixed top-0 inset-x-0 z-40'>
-      <div className='flex row mx-6 items-center'>
-        <BtnMenu classes='z-30 relative ml-auto pt-6' />
+      <div className='flex row mx-30 items-center'>
+        <BtnMenu classes='z-30 relative ml-auto pt-30' />
       </div>
     </section>
   );
