2ddd3716a89454f1ee12639c5dd241b08c042a9a
Robin Pillar <rpillar72@gmail.com>
2021-05-13 20:20:04 +0100

    fixed menu position on initial load

--- components/site/MobileMenu.jsx
+++ components/site/MobileMenu.jsx
@@ -17,8 +17,8 @@ export default function MobileMenu() {
   return (
     <section
       className={classNames(
-        'site-mobileMenu fixed h-screen w-full inset-y-0 right-0 transition-transform duration-500 bg-gradient-to-br from-teal-400 to-teal-600 z-30 max-w-450 cursor-dark',
-        { 'transform translate-x-full': !menuOpen }
+        'site-mobileMenu fixed h-screen w-full inset-y-0 left-full transform transition-transform duration-500 bg-gradient-to-br from-teal-400 to-teal-600 z-30 max-w-450 cursor-dark',
+        { '-translate-x-full': menuOpen }
       )}
     >
       <div className='px-40 py-80 flex flex-col items-start space-y-60 mt-50 text-white'>
