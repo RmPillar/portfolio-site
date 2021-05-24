22f05b0d28492097247265c030cc996621b5e043
Robin Pillar <rpillar72@gmail.com>
2021-05-24 09:04:52 +0100

    added head

--- components/site/Page.jsx
+++ components/site/Page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
+
 import Header from './Header';
 import MobileMenu from './MobileMenu';
 
@@ -14,6 +16,10 @@ const Cursor = dynamic(() => import('./Cursor'), {
 function Page({ children, locomotiveOn = false }) {
   return (
     <section className='site-page flex flex-col h-screen min-h-screen'>
+      <Head>
+        <title>Robin Pillar</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Header />
       <MobileMenu />
       {locomotiveOn && <Locomotive>{children}</Locomotive>}
